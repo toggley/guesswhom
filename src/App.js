8e61af86f257e193b8be968c1b7aa2bd5506b049
iamaustinsy <iamaustinsy@gmail.com>
2022-03-05 13:25:32 -0800

    Adding my name to app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Brian commit
+          Brian commit,
+          Austin Commit
         </p>
         <a
           className="App-link"
