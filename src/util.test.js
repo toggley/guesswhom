d3d872ed1bacccf24a526123b4134f91533aaae5
NguyenBrianL97 <NguyenBrianL97@gmail.com>
2022-03-27 12:30:42 -0700

    ID-46: add prettier

--- src/util.test.js
+++ src/util.test.js
@@ -1,8 +1,8 @@
-describe('jest foo example', () => {
-  test('foo: 2 + 2 = 4', () => {
-    let sum = 0
-    sum += 2
-    sum += 2
+describe("jest foo example", () => {
+  test("foo: 2 + 2 = 4", () => {
+    let sum = 0;
+    sum += 2;
+    sum += 2;
     expect(sum).toBe(4);
     expect(sum).not.toBe(3);
   });
